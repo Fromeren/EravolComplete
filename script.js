e82564cee6bd427f9938b8898c9ab502ee0a661b
Fromeren <mathiasfrom@gmail.com>
2024-05-15 02:04:53 +0200

    123

--- script.js
+++ script.js
@@ -11,29 +11,3 @@ function scrollToElement(elementSelector, instance = 0){
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
-
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
-})
-
-link2.addEventListener('click', () => {
-    scrollToElement('.header', 1);
-})
-
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
-})
-
-function updateTemperature(temperature) {
-    //indsæt server addresse i fetch
-    fetch('', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ temperature: temperature }),
-    })
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-}
